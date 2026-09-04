0cde3c54356ea9f93f9449fd65f6029b7fd100a4
namesanas <anaskham909654@gmail.com>
2026-09-04 18:19:26 +0530

    Add host_interface header file with initialization functions

---
+++ host_interface/inc.h
@@ -0,0 +1,16 @@
+/*
+ * host_interface.h
+ *
+ *  Created on: 04-Sept-2026
+ *      Author: Identium
+ */
+
+#ifndef INC_HOST_INTERFACE_H_
+#define INC_HOST_INTERFACE_H_
+
+#include <stdint.h>
+
+void HOST_Interface_Init(void);
+void HOST_Interface_Task(void);
+
+#endif /* INC_HOST_INTERFACE_H_ */
