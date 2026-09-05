fcbfd02bc1a2e36f0e5ec051d07dfdefacf2fbd7
namesanas <anaskham909654@gmail.com>
2026-09-05 17:55:08 +0530

    Expand SILION application API with new functions
    Added new functions for reader configuration and information retrieval.

--- silion_application/inc.h
+++ silion_application/inc.h
@@ -1,10 +1,3 @@
-/*
- * sillion_application.h
- *
- *  Created on: 04-Sept-2026
- *      Author: Identium
- */
-
 #ifndef INC_SILLION_APPLICATION_H_
 #define INC_SILLION_APPLICATION_H_
 
@@ -16,10 +9,70 @@ typedef enum
     SILION_APP_INVENTORY
 } SILION_ApplicationState_t;
 
-uint8_t SILION_Application_StartInventory(void);
-void SILION_Application_StopInventory(void);
-SILION_ApplicationState_t SILION_Application_GetState(void);
+
+/*
+ * ------------------------------------------------------------
+ * Application initialization / task
+ * ------------------------------------------------------------
+ */
 void SILION_Application_Init(Silion_Handle_t *pSilionHandle);
+
+uint8_t SILION_Application_ConfigureReader(void);
+
 void SILION_Application_Task(void);
+
+
+/*
+ * ------------------------------------------------------------
+ * Reader information
+ * ------------------------------------------------------------
+ */
+uint8_t SILION_Application_GetVersion(void);
+uint8_t SILION_Application_GetSerial(void);
+uint8_t SILION_Application_GetTemperature(void);
+uint8_t SILION_Application_GetRegion(void);
+uint8_t SILION_Application_GetAntenna(void);
+uint8_t SILION_Application_GetPower(void);
+uint8_t SILION_Application_GetProtocol(void);
+uint8_t SILION_Application_GetSession(void);
+uint8_t SILION_Application_GetFrequency(void);
+uint8_t SILION_Application_GetRegions(void);
+
+
+/*
+ * ------------------------------------------------------------
+ * Reader settings
+ * ------------------------------------------------------------
+ */
+uint8_t SILION_Application_SetPower(
+    uint16_t readPower,
+    uint16_t writePower
+);
+
+uint8_t SILION_Application_SetAntenna(
+    uint8_t txAntenna,
+    uint8_t rxAntenna
+);
+
+uint8_t SILION_Application_SetRegion(
+    uint8_t region
+);
+
+uint8_t SILION_Application_SetSession(
+    uint8_t session
+);
+
+
+/*
+ * ------------------------------------------------------------
+ * Inventory
+ * ------------------------------------------------------------
+ */
+uint8_t SILION_Application_StartInventory(void);
+
+void SILION_Application_StopInventory(void);
+
+SILION_ApplicationState_t
+SILION_Application_GetState(void);
 
 #endif /* INC_SILLION_APPLICATION_H_ */
